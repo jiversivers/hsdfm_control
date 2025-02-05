40408e8c7bba453a07fea293bde7c4de9b9d9080
jiversivers <jdivers@uark.edu>
2025-02-05 10:23:43 -0600

    added exit button to close all

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ from tkinter import messagebox
 
 from controllers.routines import record_do, capture_images, synchronized_phantom_measurement
 
+
+def close_all():
+    """Closes all Tkinter windows and exits the application."""
+    if messagebox.askokcancel("Quit", "Are you sure you want to quit?"):
+        for window in list(root.winfo_children()):
+            window.destroy()
+    root.quit()  # Exit the main event loop
+    root.destroy()  # Destroy the main root window
+
+
 root = tk.Tk()
 root.title("What do you want to do?")
 root.geometry("400x300")
@@ -16,6 +26,10 @@ button2.pack(pady=10)
 
 button3 = tk.Button(root, text="Synced Phantom Measuring", width=20, height=2, command=synchronized_phantom_measurement)
 button3.pack(pady=10)
+
+# Add a Close button
+close_button = tk.Button(root, text="Exit", width=20, height=2, command=close_all)
+close_button.pack(pady=10)
 
 # Start the Tkinter event loop
 root.mainloop()
